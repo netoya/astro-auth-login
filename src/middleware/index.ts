b42f6e0b40619d85c4ff0bd7407ed9ffcfdd89b3
Angel Oliver <angel.oliver.cv@gmail.com>
2024-01-31 08:06:23 -0300

    [middleware] Strapi user as Astro.locals.user if have token

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,4 +1,5 @@
 import { sequence } from "astro:middleware";
 import { sessionMiddleware } from "./sessionMiddleware";
+import { userMiddleware } from "./userMiddleware";
 
-export const onRequest = sequence(sessionMiddleware);
+export const onRequest = sequence(sessionMiddleware, userMiddleware);
