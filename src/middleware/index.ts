5fa42283a19bf6d0e2938f98d8e01cc3f3d92c7c
Angel Oliver <angel.oliver.cv@gmail.com>
2024-02-01 08:11:52 -0300

    [middleware] Cart Strapi Middleware.

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,5 +1,10 @@
 import { sequence } from "astro:middleware";
 import { sessionMiddleware } from "./sessionMiddleware";
 import { userMiddleware } from "./userMiddleware";
+import { cartMiddleware } from "./cartMiddleware";
 
-export const onRequest = sequence(sessionMiddleware, userMiddleware);
+export const onRequest = sequence(
+  sessionMiddleware,
+  userMiddleware,
+  cartMiddleware
+);
--- src/modules/auth/handlers/AuthController.tsx
+++ src/modules/auth/handlers/AuthController.tsx
@@ -1,62 +1,62 @@
-
 import { SessionStrapiManager } from "@functions/session-strapi";
 
 export class AuthController {
     private Astro: any;
     private session: SessionStrapiManager;
 
-    constructor(Astro){
+    constructor(Astro) {
         this.Astro = Astro;
         this.session = Astro.locals.session;
     }
 
-    async login(){
-        const sessionData = this.session.getData();
-        
+    async login() {
+        const sessionData = await this.session.getData();
+        const user = this.Astro.locals.user;
+
         if (this.Astro.request.method !== "POST") {
             return sessionData;
         }
-        
+
+        if (user) {
+            return sessionData;
+        }
+
         const data = await this.Astro.request.formData();
         const identifier = data.get("identifier");
         const password = data.get("password");
-        
+
         try {
-         
-        const resp = await fetch(`http://localhost:1337/api/auth/local`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-                "Accept": "application/json"
+            const resp = await fetch(`http://localhost:1337/api/auth/local`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    Accept: "application/json",
                 },
                 body: JSON.stringify({
                     identifier,
-                    password
-                    })
-                })
-        
-        const user= await resp.json();
-        console.log({user})
-        sessionData.user = user;
+                    password,
+                }),
+            });
 
+            const user = await resp.json();
+            console.log({ user });
+            sessionData.user = user;
         } catch (error) {
             console.log(error);
         }
 
-        this.session.save(sessionData);
+        await this.session.save(sessionData);
 
         return sessionData;
     }
-    
 
-    getUser(){
+    getUser() {
         const sessionData = this.session.getData();
         return sessionData.user;
     }
 
-
-    async logout(){
-        const sessionData = this.session.getData();
+    async logout() {
+        const sessionData = await this.session.getData();
         sessionData.user = null;
 
         this.session.save(sessionData);
